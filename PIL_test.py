6890250d0d66be861659f06c99f5ada73442746d
mimizhang <mimizhang12345@163.com>
2017-04-20 09:37:40 +0800

    PIL_test 添加tesseract数字识别

--- PIL_test.py
+++ PIL_test.py
@@ -4,9 +4,12 @@ from io import BytesIO
 from urllib.request import urlopen
 
 from requests import get
+import pyocr
 
-
-url = 'http://image.58.com/showphone.aspx?t=v55&v=EDF873EC49396CD0D16AC5BFCE65B435B'
+# http://image.58.com/showphone.aspx?t=v55&v=415A29148D7F206B2B8F5CAE15460728E
+# http://image.58.com/showphone.aspx?t=v55&v=41A2DD339188E208XBD3E27EBCAB60748
+# http://image.58.com/showphone.aspx?t=v55&v=351B5E297475CF355CAB4AF43F6C0503
+url = 'http://image.58.com/showphone.aspx?t=v55&v=33ECF6A227C1420A80E191F3146717743'
 # urllib
 image_bytes = urlopen(url).read()
 # requests
@@ -15,5 +18,21 @@ data_stream = BytesIO(image_bytes)
 pil_image = Image.open(data_stream)
 # 二值化
 pil_image = pil_image.convert('L')
+data = pil_image.getdata()
+print(data)
 print(pil_image.size)
 pil_image.show()
+
+tools = pyocr.get_available_tools()
+tool = tools[0]
+print("Will use tool '%s'" % (tool.get_name()))
+langs = tool.get_available_languages()
+print("Available languages: %s" % ", ".join(langs))
+lang = langs[2]
+print("Will use lang '%s'" % (lang))
+digits = tool.image_to_string(
+    pil_image,
+    lang=lang,
+    builder=pyocr.builders.DigitBuilder(),
+)
+print(digits.replace(' ', ''))
